04e0fd1990d26965ea5ce52782b45cde04111bf8
TeeAre3 <tomaszrezler99@gmail.com>
2023-10-24 14:59:35 +0200

    update

--- database/createDB.php
+++ database/createDB.php
@@ -11,8 +11,8 @@
     echo "Connected successfully";
     
     
-    $database="3INb_gr2";
-    //create database
+    $database="5INb_gr2";
+    // //create database
     // $sql="CREATE DATABASE $database";
     // if(mysqli_query($conn, $sql)){
     //     echo "Database created sucessfully";
@@ -53,17 +53,64 @@
     // }else{
     //     echo "Error: ".$sql."?<br>".mysqli_error($conn);
     // }
-    // mysqli_close($conn);
+    
+    // $sql = "SELECT id, firstname, lastname FROM MyGuests";
+    // $result = mysqli_query($conn, $sql);
+    
+    // if(mysqli_num_rows($result)>0){
+    //     //output data of each row
+    //     while($row = mysqli_fetch_assoc($result)){
+    //         echo "id: ".$row["id"]. " - Name: ".$row["firstname"]. " ".$row["lastname"]. "<br>"; 
+    //     }
+    // }else{
+    //     echo "0 results";
+    // }
 
-    $sql = "SELECT id, firstname, lastname FROM MyGuests";
-    $result = mysqli_query($conn, $sql);
+    // $sql = "SELECT id, firstname, lastname FROM MyGuests";
+    // $result = mysqli_query($conn, $sql);
 
-    if(mysqli_num_rows($result)>0){
-        //output data of each row
-        while($row = mysqli_fetch_assoc($result)){
-            echo "id: ".$row["id"]. " - Name: ".$row["firstname"]. " ".$row["lastname"]. "<br>"; 
-        }
+    // if (mysqli_num_rows($result)>0){
+    //     //output data of each row
+    //     while($row=mysqli_fetch_row($result)){
+    //         echo "id: ".$row[0]." - Name: ".$row[1]." ".$row[2]."<br>";
+    //     }
+    // }else{
+    //     echo "0 results";
+    // }
+
+    // $sql = "SELECT id, firstname, lastname FROM MyGuests";
+    // $result = mysqli_query($conn, $sql);
+
+    // if (mysqli_num_rows($result)>0){
+    //     echo "<table>";
+    //     while($row=mysqli_fetch_row($result)){
+    //         echo "<tr><td>".$row[0]."</td><td>".$row[1]."</td><td>".$row[2]."</td></tr>";
+    //     }
+    //     echo "</table>";
+    // }else{
+    //     echo "0 results";
+    // }
+
+    // $sql = "SELECT id, firstname, lastname FROM MyGuests";
+    // $result = mysqli_query($conn, $sql);
+
+    // if (mysqli_num_rows($result)>0){
+    //     echo "<ul>";
+    //     while($row=mysqli_fetch_row($result)){
+    //         echo "<li>".$row[0]." ".$row[1]." ".$row[2]."</li>";
+    //     }
+    //     echo "</ul>";
+    // }else{
+    //     echo "0 results";
+    // }
+
+    $sql="DELETE FROM MyGuests WHERE id=3;";
+
+    if(mysqli_query($conn,$sql)){
+        echo "Record deleted successfully";
     }else{
-        echo "0 results";
+        echo "Error deleting record: ".mysqli_error($conn);
     }
+
+    mysqli_close($conn);
 ?>
