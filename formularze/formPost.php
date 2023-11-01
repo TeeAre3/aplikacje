33ad759bcbf7365f8781a5b9b65f4938bc4fdeea
TeeAre3 <113103249+TeeAre3@users.noreply.github.com>
2023-11-01 23:09:28 +0100

    Update formPost.php

--- formularze/formPost.php
+++ formularze/formPost.php
@@ -6,7 +6,7 @@
     $username = "root"; 
     $password = "";
 
-    $conn = mysqli_connect($servername, $username, $password,);
+    $conn = mysqli_connect($servername, $username, $password);
 
     if (!$conn){
         die("Connection failed: ".mysqli_connect_error());
